7513eed696257c82f2a36c45c1c679247b6ffde8
Kym0211 <kavyam.ksingh@gmail.com>
2025-12-18 12:12:02 +0530

    added Apache-2.0 license

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 #!/usr/bin/env node
 
+/*
+ * Copyright 2025 ChainflowSol
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { signOffline } from "./commands/sign"; 
